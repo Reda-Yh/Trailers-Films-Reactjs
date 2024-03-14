4a8ab2985b5a1e08342b56d8c6a1cde21622fa1f
Reda-Yh <122675645+Reda-Yh@users.noreply.github.com>
2024-03-14 17:17:05 +0000

    Update Banner.jsx

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -14,7 +14,7 @@ import PlayBtn from '../../components/Movie/PlaybBtn/PlayBtn';
 function Banner() {
   const [movies,setmovies] = useState([])
   const fetchData = () => {
-    fetch("http://localhost:3000/data/movieData.json")
+    fetch("/data/movieData.json")
     .then(res=>res.json())
     .then(data=>setmovies(data))
     .catch(err=>console.log(err.message))
